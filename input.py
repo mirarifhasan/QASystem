b707ba1bec2eb2a058751a4271b97904080b2daa
AnikaTanzim <tanzimanika97@gmail.com>
2020-03-26 14:34:13 +0600

    ques analysis and keyword extraction

--- input.py
+++ input.py
@@ -13,8 +13,29 @@ nlp = spacy.load('en')
 
 myfile= open("questions.txt").read()
 doc_file= nlp(myfile)
-
+count=0
 #print (doc_file)
 for questions in doc_file.sents:
-    print(questions)
+    count=count+1
+    print(count,". ",questions)
+    
+    #parts of speech, tags, dependency
+    for tokens in questions:
+        print(tokens.text,"     ", tokens.pos_,"    ",tokens.tag_, "    ", tokens.dep_)
+    print("\nName Entity: ")
+    
+    #name entity recognition
+    for word in questions.ents:
+        print(word.text,word.label_)
+     
+    print("\nKeyword:")
+    keyword=[]
+    for word in questions:
+        if (word.is_stop == False) and (word.text !="?"):
+            keyword.append(word.text)
+    print(keyword)
+    print("\n\n")
+    
+
+
     
