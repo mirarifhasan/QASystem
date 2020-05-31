791fa41d377da9eda5033d8ccde094f8b18054d8
“mirarifhasan” <arif.ishan05@gmail.com>
2020-05-31 17:35:50 +0600

    answer grouping

--- q_a_system/spacy_play/answer_validation.py
+++ q_a_system/spacy_play/answer_validation.py
@@ -3,13 +3,17 @@ from q_a_system.global_pack import strings
 
 nlp = spacy.load(strings.lang)
 
-def answer_validation(answer_array, type):
-    for word in answer_array:
-        sentence = nlp(word)
+def answerValidation(answerArray, type):
+    answer = []
+    for answerGroup in answerArray:
+        sentence = nlp(answerGroup[0])
 
         for token in sentence.ents:
             #print(token.text, token.label_)
             if(token.label_ == type):
-                answer = token.text
+                if(type == 'DATE'):
+                    return answerGroup[0]
+                else:
+                    return answerGroup
 
-    return answer
+    return "No result found"
