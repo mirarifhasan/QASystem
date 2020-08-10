8989e565ecf815b525b874707fc882526b5730e7
AnikaTanzim <tanzimanika97@gmail.com>
2020-08-10 13:17:26 +0600

    wh-words

---
+++ q_a_system/spacy_play/wh_words.py
@@ -0,0 +1,36 @@
+from sqlalchemy.dialects.mysql import pymysql
+
+from q_a_system.global_pack import constant
+from q_a_system.spacy_play import parts_of_speech
+import pymysql
+
+db = pymysql.connect("localhost","Thesis","Thesis123", "QASYSTEM" )
+cursor = db.cursor()
+
+
+
+def whWord(ques):
+    question = constant.nlp(ques)
+
+    questionWord = parts_of_speech.tokenize(question)
+
+    word=""
+    if questionWord[0] in ['How' , 'What','Where', 'When','Which','Who', 'Whom', 'List']:
+        word=questionWord[0]
+
+    elif questionWord[0] in ['Show', 'Give']:
+        word= "List"
+    elif questionWord[0] in ['In','On','To'] and questionWord[1] in ['which','what']:
+        word= questionWord[1]
+    elif questionWord[0] in ['Do', 'Does', 'Did']:
+        word = 'Others'
+
+    cursor.execute("SELECT wid FROM whwords WHERE WHwords =  '" +word +" ' " )
+    results = cursor.fetchall()
+    for row in results:
+        a = row[0]
+
+
+    return a
+
+#print(whWord("When is the time"))
